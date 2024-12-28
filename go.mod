81e0cc812c6e1cf49d83206a331ec65812c7b889
Fred Baube <fbaube@iki.fi>
2024-12-28 11:08:20 +0200

    Fix compilation errors

--- go.mod
+++ go.mod
@@ -1,5 +1,34 @@
 module github.com/fbaube/rest
 
-go 1.22.3
+go 1.23
 
-require github.com/danielgtaylor/huma/v2 v2.18.0
+toolchain go1.23.4
+
+require (
+	github.com/fbaube/datarepo v0.0.0-20241228082824-469ea5670e0b
+	github.com/fbaube/m5db v0.0.0-20240509180501-84aae2bef357
+	github.com/fbaube/mlog v0.0.0-20240425064535-3b89e3b28a76
+)
+
+require (
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/fbaube/contentanalysis v0.0.0-20240924121349-9f6966a9ad43 // indirect
+	github.com/fbaube/ctoken v0.0.0-20240918123605-d4f3b42f3fa7 // indirect
+	github.com/fbaube/dsmnd v0.0.0-20241113194348-8bcb3be2decd // indirect
+	github.com/fbaube/fileutils v0.0.0-20241214171642-8f768cf51d18 // indirect
+	github.com/fbaube/humanbytes v0.0.0-20240227103335-a0b11501ad0e // indirect
+	github.com/fbaube/logutils v0.0.0-20240425064504-eb572963f0fc // indirect
+	github.com/fbaube/lwdx v0.0.0-20241028093627-8a7133fbc931 // indirect
+	github.com/fbaube/stringutils v0.0.0-20240923132734-4299299a2f4d // indirect
+	github.com/fbaube/wasmutils v0.0.0-20231209183154-e25018375bc2 // indirect
+	github.com/fbaube/xmlutils v0.0.0-20240425064631-d7c56373bd9a // indirect
+	github.com/gabriel-vasile/mimetype v1.4.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/mgutz/str v1.2.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/tools v0.28.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ runrest.go
@@ -0,0 +1,231 @@
+package rest
+
+/*
+NEW WAY:
+1) Let tbl := {table} from the request
+2) Get tbl's TblDesc
+3) Form the SQL stmt without a buffer ptr
+4) It will use the TblDesc tools to return a buffer ptr as a RowModel (or "any")
+5) Then can we print it as JSON, without ever really knowing its type ?
+
+Perhaps each RowModel also needs to provide
+its own JSON Decode & Encode functions.
+Then everything occurs in text-only, without the code
+ever knowing the specific types of the RowModels's ?
+*/
+
+import (
+	L "github.com/fbaube/mlog"
+	"net/http"
+	"strconv"
+//	S "strings"
+//	"github.com/fbaube/m5db"
+	"encoding/json"
+	DR "github.com/fbaube/datarepo"
+//	DRM "github.com/fbaube/datarepo/rowmodels"
+//	DRS "github.com/fbaube/datarepo/sqlite"
+)
+
+var sRestPortNr string
+
+var the_m5db DR.SimpleRepo
+
+// THIS IS JUST A DATASTORE !
+/*
+type taskServer struct {
+	store *taskstore.TaskStore
+}
+func NewTaskServer() *taskServer {
+	store := taskstore.New()
+	return &taskServer{store: store}
+}
+*/
+
+func RunRest(portNr int, aDB DR.SimpleRepo) error {
+	if portNr == 0 { // env.RestPort
+		return nil
+	}
+	if aDB == nil { return nil }
+	println("RUN REST")
+	sRestPortNr = strconv.Itoa(portNr)
+	the_m5db = aDB 
+	
+// 2023.10 https://eli.thegreenplace.net/2023/better-http-server-routing-in-go-122
+// 2021.01 https://eli.thegreenplace.net/2021/rest-servers-in-go-part-1-standard-library
+
+	mux := http.NewServeMux()
+/*
+	mux.HandleFunc(  "POST /{table}/",      hCreate)
+	mux.HandleFunc("DELETE /{table}/",      hDeleteAll)
+	mux.HandleFunc(   "GET /{table}/",      hGetAll)
+	mux.HandleFunc("DELETE /{table}/{id}/", hDeleteByID)
+*/
+	mux.HandleFunc(   "GET /{table}/{id}/", hGetByID)
+//	mux.HandleFunc(   "GET /tag/{tag}/",    hMultiGetByTag)
+/*
+	huma.Get(api, "/about",
+		pRsp.ContentType = "text/html"
+		sTop, eTop := GetContents("top.hf")
+		sMid, eMid := GetContents("mid.hf")
+		sAbt, eAbt := GetContents("about.hb")
+		sBtm, eBtm := GetContents("btm.hf")
+		pRsp.Body = []byte(
+			// "<!DOCTYPE html>\n<html>\n<body>\nABOUT!\n" +
+			// "</body></html>")
+			sTop + sMid + sAbt + sBtm)
+		return pRsp, cmp.Or(eTop, eMid, eAbt, eBtm, nil)
+*/
+	// fmt.Printf("API: %+v \n", api)
+	println("==> Running REST server on port:", 8888) // sRestPortNr)
+	// Start the server!
+	http.ListenAndServe("127.0.0.1:8888", mux)
+	// http.ListenAndServe("localhost:8888", mux)
+	return nil
+}
+
+// TIME ABOUT HEALTH CONTACT RSS FUNC 
+
+/*
+
+	// ADMIN
+	r.HandleFunc("/stc", hdlStcRoot)
+	// TOPICS, MAPS, DATABASE, STATIC CONTENT
+	rtrTpc := r.PathPrefix("/tpc").Subrouter()
+	rtrMap := r.PathPrefix("/map").Subrouter()
+	rtrDb := r.PathPrefix("/db").Subrouter()
+	// HOME (incl. "About", etc.)
+	r.HandleFunc("/", HomeHandler)
+
+	// TOPICS
+	rtrTpc.HandleFunc("/{id}/meta", hdlTopicMeta)
+	rtrTpc.HandleFunc("/{id}/links", hdlTopicLinks)
+	rtrTpc.HandleFunc("/{id}", hdlTopic)
+	rtrTpc.HandleFunc("/", hdlTopicRoot)
+
+	// MAPS
+	rtrMap.HandleFunc("/{id}/meta", hdlMapMeta)
+	rtrMap.HandleFunc("/{id}/links", hdlMapLinks)
+	rtrMap.HandleFunc("/{id}", hdlMap)
+	rtrMap.HandleFunc("/", hdlMapRoot)
+
+	// DB (schemas? stats?)
+	rtrDb.HandleFunc("/fld/{name}", hdlDbField)
+	rtrDb.HandleFunc("/{name}", hdlDbTable)
+	rtrDb.HandleFunc("/", hdlDbRoot)
+
+	// go func() {
+	if err := http.ListenAndServe(":"+sRestPortNr, r); err != nil {
+		L.L.Error("REST server failed: " + err.Error())
+	}
+	return nil
+}
+
+* /
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	var s string
+	s = r.RequestURI + ": " + fmt.Sprintf("home vars: %+v\n", vars)
+	/*
+		println(s)
+		ssnLog.Println(s)
+		fmt.Fprintf(w, s)
+	* /
+	L.L.Info(s)
+}
+
+func TopicRootHdlr(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	var s string
+	s = r.RequestURI + ": " + fmt.Sprintf("topic vars: %+v\n", vars)
+	/*
+		println(s)
+		ssnLog.Println(s)'
+
+		fmt.Fprintf(w, s)
+	* /
+	L.L.Info(s)
+}
+func MapRootHdlr(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	var s string
+	s = r.RequestURI + ": " + fmt.Sprintf("map vars: %+v\n", vars)
+	/*
+		println(s)
+		ssnLog.Println(s)
+		fmt.Fprintf(w, s)
+	* /
+	L.L.Info(s)
+}
+
+func StcRootHdlr(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	var s string
+	s = r.RequestURI + ": " + fmt.Sprintf("static vars: %+v\n", vars)
+	/*
+		println(s)
+		ssnLog.Println(s)
+		fmt.Fprintf(w, s)
+	* /
+	L.L.Info(s)
+
+	// This will serve files under http://localhost:8000/static/<filename>
+	// r.PathPrefix("/s/").Handler(http.StripPrefix("/s/", http.FileServer(http.Dir(dir))))
+}
+
+*/
+
+// func DRS.DoSelectByIdGeneric[T DRM.RowModel](pSR *SqliteRepo, anID int, pDest T) (bool, error) 
+
+func hGetByID(w http.ResponseWriter, req *http.Request) {
+  L.L.Info("Handling get task at %s", req.URL.Path)
+
+  tbl := req.PathValue("table")
+  id, err := strconv.Atoi(req.PathValue("id"))
+  if err != nil {
+    http.Error(w, "invalid id", http.StatusBadRequest)
+    return
+  }
+  // ok, e := DRS.DoSelectByIdGeneric(the_m5db, id, typedBuf)
+  rq := new(DR.RestQuery)
+  rq.DBOp = "get"
+  rq.Table = tbl
+  rq.Id1 = id
+  
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusNotFound)
+    return
+  }
+
+  //renderJSON(w, task)
+}
+
+// func DRS.DoInsertGeneric[T DRM.RowModel](pSR *SqliteRepo, pRM T) (int, error) 
+
+/*
+The only handler that's a bit special is hCreate, since it
+has to parse JSON data sent by the client in the request body.
+There are some nuances to JSON parsing in requests that I did
+not cover - check out this post for a more thorough approach.
+https://www.alexedwards.net/blog/how-to-properly-parse-a-json-request-body
+
+*/
+
+// Hyper-robust: How to Parse a JSON Request Body in Go
+// https://www.alexedwards.net/blog/how-to-properly-parse-a-json-request-body
+
+// renderJSON renders 'v' as JSON and writes it as a response into w.
+func renderJSON(w http.ResponseWriter, v interface{}) {
+  js, err := json.Marshal(v)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(js)
+}
+
